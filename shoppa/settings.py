5553d72d03f66bf9dcec0889676e32fb7ebe4735
LeifAG <leif.hammar@gymnasium.ax>
2022-11-13 23:12:50 +0200

    setup

--- shoppa/settings.py
+++ shoppa/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#p%7p)+5++@m93*=6t@8t8e(d%rhk_2i7ese)n%#bau!lvdl$9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
